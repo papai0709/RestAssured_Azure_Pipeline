73fc6cc49152160c4861c631b5a1d02470b288c6
papai0709 <jay.papai007@gmail.com>
2025-03-28 20:52:06 +0530

    Fetch data from Cosmos Db Successful.!

--- src/main/java/CreateDbConnection.java
+++ src/main/java/CreateDbConnection.java
@@ -1,12 +1,8 @@
-import com.azure.cosmos.CosmosClient;
-import com.azure.cosmos.CosmosClientBuilder;
-import com.azure.cosmos.CosmosContainer;
-import com.azure.cosmos.CosmosDatabase;
+import com.azure.cosmos.*;
 import com.azure.cosmos.models.CosmosContainerResponse;
 import com.azure.cosmos.models.CosmosDatabaseResponse;
 import com.azure.cosmos.models.CosmosQueryRequestOptions;
-import com.azure.cosmos.models.PartitionKey;
-import com.azure.cosmos.util.CosmosPagedIterable;
+import com.azure.cosmos.util.CosmosPagedFlux;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,9 +12,9 @@ import java.util.Properties;
 public class CreateDbConnection {
 
     static Properties properties = new Properties();
+    static CosmosAsyncClient client;
 
-
-    public static CosmosContainer connect() throws IOException {
+    public static CosmosAsyncContainer connect() throws IOException {
         System.out.println("Reading Cosmos DB configuration");
         properties.load(Files.newInputStream(Paths.get("src/main/resources/data.properties")));
         String ENDPOINT = properties.getProperty("ENDPOINT");
@@ -28,47 +24,57 @@ public class CreateDbConnection {
 
         System.out.println("Connecting to Cosmos DB");
 
-        // Create a new CosmosClient
-        CosmosDatabase database;
-        try (CosmosClient client = new CosmosClientBuilder()
+        // Create a new CosmosAsyncClient
+        client = new CosmosClientBuilder()
                 .endpoint(ENDPOINT)
                 .key(KEY)
-                .buildClient()) {
+                .buildAsyncClient();
 
-            // Create or get the database
-            CosmosDatabaseResponse databaseResponse = client.createDatabaseIfNotExists(DATABASE_NAME);
-            database = client.getDatabase(databaseResponse.getProperties().getId());
-        }
+        // Create or get the database
+        CosmosDatabaseResponse databaseResponse = client.createDatabaseIfNotExists(DATABASE_NAME).block();
+        assert databaseResponse != null;
+        CosmosAsyncDatabase database = client.getDatabase(databaseResponse.getProperties().getId());
 
         // Create or get the container
-        CosmosContainerResponse containerResponse = database.createContainerIfNotExists(CONTAINER_NAME, "/partitionKey");
-        CosmosContainer container = database.getContainer(containerResponse.getProperties().getId());
+        CosmosContainerResponse containerResponse = database.createContainerIfNotExists(CONTAINER_NAME, "/partitionKey").block();
+        assert containerResponse != null;
+        CosmosAsyncContainer container = database.getContainer(containerResponse.getProperties().getId());
 
         System.out.println("Connected to Cosmos DB");
         return container;
-
     }
 
-
-    public static void readDataFromCosmosDb(CosmosContainer container) {
+    public static void readDataFromCosmosDb(@org.jetbrains.annotations.NotNull CosmosAsyncContainer container) {
         System.out.println("Reading data from Cosmos DB");
 
         String sqlQuery = "SELECT * FROM c WHERE c.id = 'Emp_1001'";
         CosmosQueryRequestOptions options = new CosmosQueryRequestOptions();
-        options.setPartitionKey(new PartitionKey("id"));
 
-        CosmosPagedIterable<Object> items = container.queryItems(sqlQuery, options, Object.class);
+        CosmosPagedFlux<Object> items = container.queryItems(sqlQuery, options, Object.class);
+        items.byPage().subscribe(page -> {
+            System.out.println("Data read from Cosmos DB");
+            page.getElements().forEach(System.out::println);
+            closeConnection();
+        });
+    }
 
-        items.forEach(item -> System.out.println(item));
+    public static void closeConnection() {
+        if (client != null) {
+            client.close();
+            System.out.println("Connection to Cosmos DB closed");
+        }
     }
 
     public static void main(String[] args) {
-//        CreateCosmosConnection cosmosDbConnection = new CreateCosmosConnection();
         try {
-            CosmosContainer container = connect();
+            CosmosAsyncContainer container = connect();
             readDataFromCosmosDb(container);
         } catch (IOException ignored) {
-
+            System.out.println("Error reading properties file");
+        } catch (Exception e) {
+            System.out.println("Error connecting to Cosmos DB: " + e.getMessage());
+        } finally {
+            System.out.println("Finished");
         }
     }
 }
